023a6bbbf4d2510853bf35e00dcfb7bf14ce7311
jhazlitt <bagelday@gmail.com>
2015-12-15 14:49:39 -0700

    Second version of bird movement, also rotates the bird downward

--- floppy_bird.js
+++ floppy_bird.js
@@ -1,5 +1,6 @@
 var velocity = 1;
-var birdAltitude = 0;
+var birdAltitude = 10;
+var totalRotatedDegrees = 0;
 
 $(document).ready(moveBird);
 
@@ -7,6 +8,8 @@ function moveBird(){
 	$(document).keydown(function(evt) {
 		if (evt.keyCode === 32) {
 			if (birdAltitude - 100 >= 0) {
+				totalRotatedDegrees = 0;
+				rotateBird(totalRotatedDegrees);
 				$('#bird').animate({ top: "-=100" }, 50, "linear");
 				velocity = 1;
 			}
@@ -15,13 +18,34 @@ function moveBird(){
 
 	setInterval(function(){
 		birdAltitude = $('#bird').position().top;	
-		$('#bird').text(birdAltitude);
+
+		if (birdAltitude >= 570) {
+			$('#bird').stop();
+			$('#bird').hide();
+		}
+		else if (birdAltitude < -5) {
+			totalRotatedDegrees = 0;
+			rotateBird(totalRotatedDegrees);
+			$('#bird').stop();
+			$('#bird').css( "top" , "0px" );
+		}
 	},1);
 	
 	setInterval(function(){
-		if ((birdAltitude < 570) && ((birdAltitude + (velocity + 5)) < 570)) {
+//		if ((birdAltitude < 570) && ((birdAltitude + (velocity + 5)) < 570)) {
+			if (totalRotatedDegrees < 90) {
+				totalRotatedDegrees += 5;
+			}
+			rotateBird(totalRotatedDegrees);
 			$('#bird').animate({ top: "+=" + velocity + "" }, 100, "linear");
 			velocity = velocity + 5;
-		}
+//		}
 	},100);
+}
+
+function rotateBird(degrees){
+	$('#bird').css('-moz-transform', 'rotate(' + degrees + 'deg)');
+	$('#bird').css('-webkit-transform', 'rotate(' + degrees + 'deg)');
+	$('#bird').css('-o-transform', 'rotate(' + degrees + 'deg)');
+	$('#bird').css('-ms-transform', 'rotate(' + degrees + 'deg)');
 }
